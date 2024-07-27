afcfd2df2b8c2fac51857ccd7f0f383132cb8e30
Anuragsamadhiya <125735588+Anuragsamadhiya@users.noreply.github.com>
2024-07-27 23:15:18 +0530

    Design update

--- weather.js
+++ weather.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const apikey="881946e59782c25bdb85210ad617ed93";
 const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchbtn=document.querySelector(".searchbtn");
@@ -16,3 +17,23 @@ document.querySelector(".nn").style.display="inline";}
 searchbtn.addEventListener("click",()=>{
     checkweather(inputvalue.value);
 })
+=======
+const apikey="881946e59782c25bdb85210ad617ed93";
+const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchbtn=document.querySelector(".searchbtn");
+const inputvalue=document.querySelector(".search input");
+const speeds=document.querySelector(".speed");
+async function checkweather(city){
+    const response=await fetch(apiurl + city + `&appid=${apikey}`);
+    var data= await response.json();
+    // console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+"°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity+"%";
+    document.querySelector(".speed").innerHTML=data.wind.speed+" Km/h";
+    document.querySelector(".weather").style.display="block";
+document.querySelector(".nn").style.display="inline";}
+searchbtn.addEventListener("click",()=>{
+    checkweather(inputvalue.value);
+})
+>>>>>>> b04ab39 (Design update)
